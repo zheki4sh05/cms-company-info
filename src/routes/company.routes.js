1fea231a617466013890bc6763051e3c5109456b
Шостак Евгений Артурович <yauheni.shostak@softclub.by>
2026-04-17 12:12:54 +0300

    modify

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -3,6 +3,7 @@ const companyController = require("../controllers/company.controller");
 
 const router = Router();
 
+router.get("/id/:userId", companyController.getCompanyIdByUserId);
 router.get("/", companyController.getCompany);
 router.patch("/", companyController.patchCompany);
 router.delete("/", companyController.deleteCompany);
